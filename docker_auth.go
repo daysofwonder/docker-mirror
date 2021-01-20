05394d39e1add6053cc0f282b92ebadd92c13196
Charles Teinturier <charles@daysofwonder.com>
2021-01-20 17:31:19 +0100

    Add ecr helper support

--- docker_auth.go
+++ docker_auth.go
@@ -5,20 +5,34 @@ package main
 import (
 	"fmt"
 
-	"github.com/fsouza/go-dockerclient"
+	docker "github.com/fsouza/go-dockerclient"
 	log "github.com/sirupsen/logrus"
 )
 
 func getDockerCredentials(registry string) (*docker.AuthConfiguration, error) {
+	dockerlog := log.WithField("registry", registry)
+
+	// Creds from docker config file
+	dockerlog.Debug("Get credentials from docker config file")
 	authOptions, err := docker.NewAuthConfigurationsFromDockerCfg()
 	if err != nil {
-		log.Fatal(err)
+		dockerlog.Debug("No docker configuration found")
+	} else {
+		creds, ok := authOptions.Configs[registry]
+		if ok {
+			return &creds, nil
+		}
+		dockerlog.Debug("No creds found")
 	}
 
-	creds, ok := authOptions.Configs[registry]
-	if !ok {
-		return nil, fmt.Errorf("No auth found for %s", registry)
+	// Creds from docker credentials helpers
+	dockerlog.Debug("Get credentials from docker credentials helpers")
+	creds, err := docker.NewAuthConfigurationsFromCredsHelpers(registry)
+	if err != nil {
+		dockerlog.Debug(err)
+	} else {
+		return creds, nil
 	}
 
-	return &creds, nil
+	return nil, fmt.Errorf("No auth found for %s", registry)
 }
